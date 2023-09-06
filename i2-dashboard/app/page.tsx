59d568d3d81d5cba7a7422162271f1b7ac28c198
Kevin Ngkaion <kevinngkaion@gmail.com>
2023-09-06 13:14:20 -0700

    Testing Vercel autodeploy

--- i2-dashboard/app/page.tsx
+++ i2-dashboard/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <h1>
         This is will be the frontend for i2-dashboard
+
+        I made a change to see if it will auto update on Vercel.
       </h1>
     </main>
   )
