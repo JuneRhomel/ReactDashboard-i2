d1e2a3bf673c999428d8efce52b81dc1c3c070fa
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2023-09-13 09:08:28 +0800

    Navigation Done

--- i2-dashboard/components/layouts/navigation/Navigation.tsx
+++ i2-dashboard/components/layouts/navigation/Navigation.tsx
@@ -1,2 +1,41 @@
-import styles from './Navigation.module.css' ;
+import styles from './Navigation.module.css';
+import { MdDashboard, MdBallot, MdReceipt, MdInsertChart, MdMenu } from "react-icons/md";
+import React, { useState } from 'react'
+import { useRouter } from 'next/router';
 
+
+export default function Navigation() {
+    const [selectedTab, setSelectedTab] = useState('dashboard'); 
+    const router = useRouter();
+
+    const navigationItems = [
+        { id: 1, link: 'dashboard', text: 'Dashboard', icon: <MdDashboard /> },
+        { id: 2, link: 'soa', text: 'SOA', icon: <MdBallot /> },
+        { id: 3, link: 'my-request', text: 'My Request', icon: <MdReceipt /> },
+        { id: 4, link: 'news-announcement', text: 'News & Announcement', icon: <MdInsertChart /> },
+        { id: 5, link: 'menu', text: 'Menu', icon: <MdMenu /> },
+    ];
+
+    const navigate = (id: string) => {
+        setSelectedTab(id); 
+        // router.push(`/${id}`);
+    };
+
+    return (
+        <nav className={styles.navigation}>
+            <ul>
+                {navigationItems.map((item) => (
+                    <li key={item.id}>
+                        <a
+                            onClick={() => navigate(item.link)}
+                            className={`${styles.tab} ${selectedTab === item.link ? styles.active : ''}`}
+                        >
+                            <div className={styles.icons}>{item.icon}</div>
+                            {item.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
--- i2-dashboard/pages/index.tsx
+++ i2-dashboard/pages/index.tsx
@@ -1,10 +1,15 @@
 import Layout from '../components/layouts/layout';
 import Login from "./login";
+import Navigation from '@/components/layouts/navigation/Navigation';
 
 const pageTitle: string = "Welcome to Inventi I2";
 
 export default function Home() {
   return (
-      <Login/>
+    <>
+    <Login/>
+    <Navigation/>
+    
+    </>
   )
 }
