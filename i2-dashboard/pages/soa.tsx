f1d29e286ea90fa539a20f98afa1382f1de1f825
kevinngkaion <kevinngkaion@gmail.com>
2023-10-08 21:25:57 -0700

    Resolve conflict on /pages/soa - getServerSideProps already made.

--- i2-dashboard/pages/soa.tsx
+++ i2-dashboard/pages/soa.tsx
@@ -1,3 +1,12 @@
-import Soa from "@/components/pages/soa/Soa";
+import React from 'react'
+
+export function getServerSideProps({context}) {
+
+  //Fetch all the soa objects.
+  // Filter what I need here
+  return {
+    props: {}
+  }
+}
 
 export default Soa
