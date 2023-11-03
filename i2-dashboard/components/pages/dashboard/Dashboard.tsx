4ed897d25c241e191b577ecf3cf37b805e1aa5aa
kevinngkaion <kevinngkaion@gmail.com>
2023-11-02 17:46:25 -0700

    Changed Dashboard.tsx to filter out only the first 4 payment transactions to show

--- i2-dashboard/components/pages/dashboard/Dashboard.tsx
+++ i2-dashboard/components/pages/dashboard/Dashboard.tsx
@@ -2,17 +2,19 @@ import Section from "@/components/general/section/Section";
 import Layout from "@/components/layouts/layout";
 import { SoaPaymentsType, SoaType } from "@/types/models";
 
-export default function Dashboard({currentSoa, soaDetails}: {currentSoa: SoaType, soaDetails: SoaPaymentsType}) {
+export default function Dashboard({currentSoa, soaDetails}: {currentSoa: SoaType, soaDetails: SoaPaymentsType[]}) {
+    const first4SoaDetails = soaDetails.slice(0,4);
+
     const soaProps = {
         title: 'SOA',
         headerAction: null,
         data: {currentSoa, soaDetails},
-      }
+    }
     
     const paymentTransactionsProps = {
     title: 'Payment Transactions',
     headerAction: "Show All",
-    data: soaDetails,
+    data: first4SoaDetails,
     };
 
     const serviceRequestProps = {
