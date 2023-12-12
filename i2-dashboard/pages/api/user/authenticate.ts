5f1d61eacfd098999ed4cc1656f24067389f1ad0
June Rhomel <junemandigma@gmail.com>
2023-12-12 11:17:31 +0800

    Fetch error

--- i2-dashboard/pages/api/user/authenticate.ts
+++ i2-dashboard/pages/api/user/authenticate.ts
@@ -7,40 +7,57 @@ import getCookieString from "@/utils/getCookieString";
 import parseObject from "@/utils/parseObject";
 
 const baseURL: string = "https://apii2-sandbox.inventiproptech.com";
-const url: string = `${baseURL}/tenant/authenticate`;
-const jwtSecret : Secret = process.env.JWT_SECRET as Secret;
+const authenticateURL: string = `${baseURL}/tenant/authenticate`;
+const getUserURL: string = `${baseURL}/tenant/get-user`;
+const jwtSecret: Secret = process.env.JWT_SECRET as Secret;
+
+const fetchWithTimeout = async (url: string, options: RequestInit, timeout: number): Promise<Response> => {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+    try {
+        const response = await fetch(url, { ...options, signal: controller.signal });
+        return response;
+    } finally {
+        clearTimeout(timeoutId);
+    }
+};
 
 const authenticate = async (req: NextApiRequest, res: NextApiResponse) => {
     const body = JSON.stringify(req.body);
     const headers: HeadersInit = copyHeaders(req);
-
     try {
-        //authenticate the user
-        const response = await fetch(url, {
+        // Authenticate the user
+        const response = await fetchWithTimeout(authenticateURL, {
             method: req.method,
             body: body,
             headers: headers,
-            referrerPolicy: "unsafe-url"
-        })
-        const jsonResponse: ApiResponse<UserType> = await response.json()
+            referrerPolicy: "unsafe-url",
+        }, 15000);
+
+        const jsonResponse: ApiResponse<UserType> = await response.json();
+
         if (jsonResponse.success) {
             // Create a jwt with the user info
             const user: UserType = jsonResponse?.data as UserType;
             const body = {
                 accountcode: req.body.accountcode,
                 view: 'users',
-            }
+            };
+
             headers.Authorization = `Bearer ${user.token}:tenant`;
-            //fetch the user using their authorization token
-            const response = await fetch(`${baseURL}/tenant/get-user`, {
+            // Fetch the user using their authorization token
+            const userResponse = await fetchWithTimeout(getUserURL, {
                 method: req.method,
                 body: JSON.stringify(body),
                 headers: headers,
                 referrerPolicy: 'unsafe-url',
-            })
-            if (response.ok) {
-                const responseBody = await response.json();
+            }, 15000);
+
+            if (userResponse.ok) {
+                const responseBody = await userResponse.json();
                 const rawUser = parseObject(responseBody) as any;
+
                 //create this new User object with only relevant fields needed
                 const payload: UserType = {
                     id: rawUser.id,
@@ -59,12 +76,20 @@ const authenticate = async (req: NextApiRequest, res: NextApiResponse) => {
                     tmp: user.tmp,
                     isAuthorized: true,
                 }
-                const token: string = jwt.sign(payload, jwtSecret, {expiresIn: '1d'})
+                const token: string = jwt.sign(payload, jwtSecret, { expiresIn: '1d' });
                 const cookie: string = getCookieString(token);
-                //respond with 200 status and with the jwt set as a cookie
-                res.status(200)
-                    .setHeader("Set-Cookie", cookie)
-                    .json(payload);
+
+                // Respond with 200 status and with the jwt set as a cookie
+                res.status(200).setHeader("Set-Cookie", cookie).json(payload);
+            } else {
+                // If success==0 or response.success does not exist
+                const errorMessage = jsonResponse.status ? "Invalid account code" : "Invalid username and/or password";
+                const error = {
+                    "error": "Unauthorized",
+                    "message": errorMessage,
+                };
+
+                res.status(401).setHeader("Set-Cookie", getCookieString("Invalid", -1)).json(error);
             }
         } else {
             // If success==0 or response.success does not exist
@@ -78,11 +103,11 @@ const authenticate = async (req: NextApiRequest, res: NextApiResponse) => {
                 .json(error);
         }
 
-    } catch (error){
-        console.error(error)
-        res.status(500).json({ error: "Internal Server Error" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error -- " + error });
     }
     return;
-}
+};
 
 export default authenticate;
